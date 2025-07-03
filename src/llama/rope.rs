1ade9ff2e481c043ec478c27d13e13591bab3ee2
Yaguang Huangfu <lucienhuangfu@outlook.com>
2025-07-03 10:31:08 +0800

    powf

--- src/llama/rope.rs
+++ src/llama/rope.rs
@@ -3,15 +3,26 @@ use std::ops::{Add, Sub, Div, Mul, AddAssign, Neg };
 
 use itertools::Itertools;
 use std::iter::zip;
-// use num_traits::Float;
-// use num::complex::Complex;
 
 use crate::kernel::generic::from_usize::FromUsize;
 use crate::kernel::generic::powf::Powf;
 
+
+
+fn complex_to_polar<T>(re: T, im: T) -> (T, T)
+where
+    T: Copy + From<f64> + Into<f64> + PartialOrd,
+{
+    let re_f: f64 = re.into();
+    let im_f: f64 = im.into();
+    let r = (re_f * re_f + im_f * im_f).sqrt();
+    let theta = f64::atan2(im_f, re_f);
+    (T::from(r), T::from(theta))
+}
+
 pub fn precompute_freqs_cis<T>(dim: usize, 
             max_sequence_length: usize, 
-            theta: T) -> Vec<T> 
+            theta: f64) -> Vec<T> 
 where T: Copy 
     + Default 
     + Sub<Output = T>
@@ -20,26 +31,25 @@ where T: Copy
     + Mul<Output = T>
     + FromUsize
     + Powf
-   
+    + From<f64>
+    + Into<f64>
     {
     let end = max_sequence_length * 2;
     let freqs: Vec<_> = (0..dim).step_by(2).map(
         |x| {
             let p =   T::from_usize(x)  / T::from_usize(dim) ;
             // theta.powf(p).recip()
-            1 / T::powf(p)
+            T::from_usize(1) / T::powf(theta, p)
         }
     ).collect();
     let t: Vec<_> = (0..end).collect();
     let freqs: Vec<_> = t.into_iter().cartesian_product(freqs.into_iter()).map(|(a, b)| T::from_usize(a)*b).collect();
     let ones = vec![T::from_usize(1); freqs.len()];
     let freqs_cis: Vec<_> = zip(ones, freqs).map(|(a, b)| {
-        // let a= T::from(a).unwrap();
-        // Complex::new(a, b.into()).to_polar()
-    }).flat_map(|tup| vec![tup.0.clone(),tup.1]).collect();
-    
+        let (r, theta) = complex_to_polar(a, b);
+        (r, theta)
+    }).flat_map(|tup| vec![tup.0.clone(), tup.1]).collect();
     freqs_cis
-
 }
 
 #[cfg(test)]
