3c8311b50245601cf5903be876cb88a830092922
Yaguang Huangfu <lucienhuangfu@outlook.com>
2025-07-03 13:55:01 +0800

    finsh rope

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -25,16 +25,13 @@ pub fn build_graph(config: &Config,
     operator_queue: Rc<RefCell<Vec<Operator<f16>>>>,
     ) -> Tensor<f16> {
     let cpu_num = num_cpus::get();
-
     let word_embedding = Tensor::zeros(vec![config.vocab_size, config.hidden_size], String::from("model.word_embedding.weight"), cache.clone(), operator_queue.clone());
-    let position_embedding = Tensor::zeros(vec![config.max_position_embeddings, 1, 1, config.attention_head_size], String::from("model.position_embedding.weight"), cache.clone(), operator_queue.clone());
     let dim =  config.attention_head_size / 2;
-    let rope_vec = precompute_freqs_cis(dim, config.max_position_embeddings, 10000.0f64);
-    // let rope_embedding = Tensor::from_vec(rope_vec, String::from("model.rope_embedding.weight"), cache.clone(), operator_queue.clone());
+    let rope_vec = precompute_freqs_cis(dim, config.max_position_embeddings, 10000.0f32);
     let position_embedding = Tensor::zeros(vec![config.max_position_embeddings, 1, 1, config.attention_head_size], String::from("model.position_embedding.weight"), cache.clone(), operator_queue.clone());
     for i in 0..rope_vec.len() {
         unsafe {
-            position_embedding.data.add(i).write(rope_vec[i]);
+            position_embedding.data.add(i).write(rope_vec[i] as f16);
         }
     }
     let norm_weight = Tensor::zeros(vec![1, config.hidden_size], String::from("model.norm.weight"), cache.clone(), operator_queue.clone());
