0af0c62fbba1add3b38db5373c295c1a320b6629
Yaguang Huangfu <lucienhuangfu@outlook.com>
2025-08-14 20:00:03 +0800

    add sequence to map

--- src/compiler/map/chunk_map.rs
+++ src/compiler/map/chunk_map.rs
@@ -44,6 +44,27 @@ pub fn chunk_map<T>(
                 }
                 tasks
             }
+            4 => {
+                for i in 0..shapes[0] {
+                    let input_stride0 = input_data.add(i * strides[0]);
+                    let output_stride0 = output_data.add(i * strides[0]);
+                    for j in 0..shapes[1] {
+                        let input_stride1 = input_stride0.add(j * strides[1]);
+                        let output_stride1 = output_stride0.add(j * strides[1]);
+                        for k in 0..shapes[2] {
+                            let input_stride2 = input_stride1.add(k * strides[2]);
+                            let output_stride2 = output_stride1.add(k * strides[2]);
+                            tasks.push((
+                                ConstPtr { ptr: input_stride2 },
+                                MutPtr {
+                                    ptr: output_stride2,
+                                },
+                            ));
+                        }
+                    }
+                }
+                tasks
+            }
             _ => {
                 panic!()
             }
@@ -108,6 +129,26 @@ mod test {
         let tasks = chunk_map(shapes, strides, data.as_ptr(), output.as_mut_ptr());
         for (input, output) in tasks {
             fun(input, output, head_size);
+        }
+        //println!("{:?}", output);
+    }
+
+    #[test]
+    fn test4dimension() {
+        let inner_size = 32;
+        let head_size = 128;
+        let head_num = 64;
+        let batch_size = 16;
+        let sequence_length = 256;
+
+        let shapes = vec![batch_size, head_num, head_size, inner_size];
+        let length = shapes.iter().product();
+        let strides = get_strides(&shapes);
+        let data = vec![1.0; length];
+        let mut output = vec![0.0; length];
+        let tasks = chunk_map(shapes, strides, data.as_ptr(), output.as_mut_ptr());
+        for (input, output) in tasks {
+            fun(input, output, inner_size);
         }
         //println!("{:?}", output);
     }
