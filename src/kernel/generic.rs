2e2059738e4eef0360b65b554261f06bce76fce0
Yaguang Huangfu <lucienhuangfu@outlook.com>
2025-11-10 11:30:12 +0800

    update softmax

--- src/kernel/generic.rs
+++ src/kernel/generic.rs
@@ -13,10 +13,12 @@ pub mod argmax;
 pub mod dot_product;
 pub mod silu;
 pub mod experts_topk_softmax_norm;
-pub mod topk_softmax;
+pub mod full_topk_softmax;
 pub mod rms_norm;
 pub mod matmul_block;
 pub mod flash_attention;
+
+pub mod truncated_topk_softmax;
 
 // pub mod mat_vec_mul;
 
